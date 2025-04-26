ba5c95e2e385394f121159c991aac49a54692228
xo1ee <126421239+xo1ee@users.noreply.github.com>
2025-04-25 22:32:36 -0700

    map markers can be added and removed

--- solo-traveler/src/app/map/page.js
+++ solo-traveler/src/app/map/page.js
@@ -1,25 +1,51 @@
 "use client";
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { useState } from "react";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 const containerStyle = {
-  width: '100%',
-  height: '400px'
+  width: "100%",
+  height: "400px",
 };
 
 const center = {
   lat: 47.6062,
-  lng: -122.3321
+  lng: -122.3321,
 };
 
 export default function MyMap() {
+  const [markers, setMarkers] = useState([]);
+
+  const handleMapClick = (event) => {
+    setMarkers((current) => [
+      ...current,
+      {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+      },
+    ]);
+  };
+
+  const handleMarkerClick = (indexToRemove) => {
+    setMarkers((current) =>
+      current.filter((_, index) => index !== indexToRemove)
+    );
+  };
+
   return (
     <LoadScript googleMapsApiKey="AIzaSyB6PE_cZgir__Svb0fbN_o6c1YWdXZql-I">
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
+        onClick={handleMapClick}
         zoom={10}
       >
-        {/* Add markers, etc. here */}
+        {markers.map((marker, index) => (
+          <Marker
+            key={index}
+            position={marker}
+            onClick={() => handleMarkerClick(index)}
+          />
+        ))}
       </GoogleMap>
     </LoadScript>
   );
