340fee8dd294818c2e8cda83af00a09d04906474
xo1ee <126421239+xo1ee@users.noreply.github.com>
2025-04-26 10:44:45 -0700

    removed old react-maps api and set up google auth api

--- solo-traveler/src/app/navbar.js
+++ solo-traveler/src/app/navbar.js
@@ -42,7 +42,7 @@ export default function navbar() {
             <div className="sm:flex sm:gap-4">
               <a
                 className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow-sm"
-                href="#"
+                href="/login"
               >
                 Login
               </a>
