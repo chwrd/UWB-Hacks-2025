6fa776dd5f48db66115cb3c8ce5b34fb464c8bc6
chwrd <hocheng06@gmail.com>
2025-04-27 00:14:46 -0700

    updated styling

--- soloGo/src/app/navbar.js
+++ soloGo/src/app/navbar.js
@@ -27,31 +27,31 @@ export default function navbar() {
               <ul className=" flex gap-6 text-sm ">
                 <li>
                   <Link
-                    className="text-gray-500 transition hover:text-gray-500/75"
+                    className="text-lg text-gray-700 transition hover:text-gray-700/75"
                     href="CultureCompass"
                   >
                     {" "}
-                    Culture Compass{" "}
+                    CultureCompass{" "}
                   </Link>
                 </li>
 
                 <li>
                   <Link
-                    className="text-gray-500 transition hover:text-gray-500/75"
+                    className="text-lg text-gray-700 transition hover:text-gray-700/75"
                     href="GlobeGuard"
                   >
                     {" "}
-                    Globe Guard{" "}
+                    GlobeGuard{" "}
                   </Link>
                 </li>
 
                 <li>
                   <Link
-                    className="text-gray-500 transition hover:text-gray-500/75"
+                    className="text-lg text-gray-700 transition hover:text-gray-700/75"
                     href="SoloNet"
                   >
                     {" "}
-                    Solo Net{" "}
+                    SoloNet{" "}
                   </Link>
                 </li>
               </ul>
@@ -61,7 +61,7 @@ export default function navbar() {
           <div className="flex items-center gap-4">
             <div className="sm:flex sm:gap-4">
               <Link
-                className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow-sm"
+                className="rounded-md bg-teal-600 hover:bg-gray-700/75 px-5 py-2.5 text-sm font-medium text-white shadow-sm"
                 href="/login"
               >
                 Login
@@ -69,7 +69,7 @@ export default function navbar() {
 
               <div className="hidden sm:flex">
                 <Link
-                  className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600"
+                  className="rounded-md bg-gray-100 hover:bg-teal-600/75 hover:text-white px-5 py-2.5 text-sm font-medium text-teal-600"
                   href="#"
                 >
                   Register
--- soloGo/src/app/page.js
+++ soloGo/src/app/page.js
@@ -13,7 +13,7 @@ export default function Page() {
     <>
       {/* Introduction */}
 
-      <div className="mt-30 flex-col min-h-screen">
+      <div className="my-30 flex-col">
         <div className={`${playfairDisplay.className} text-6xl text-center`}>
           <p>
             Books teach you, <br></br> but roads show you
@@ -26,12 +26,22 @@ export default function Page() {
           height={1000}
           className="mx-auto mt-15"
         />
+
       </div>
 
+      {/* Site Summary */}
+      <section className="bg-gray-100">
+        <div className="py-40 max-w-4xl mx-auto px-6">
+          <h2 className="text-3xl font-bold text-center text-gray-800 mb-7">Explore the World with SoloGo</h2>
+          <p className="text-lg text-gray-700 text-center leading-relaxed">
+          SoloGo is your passport to bold adventures around the world. Stay informed with real-time safety updates, uncover AI-powered cultural insights personalized to your journey, and forge connections with fellow travelers as you chart your own path across the globe.
+          </p>
+        </div>
+      </section>
 
       {/* GlobeGuard Content */}
       <section>
-        <div className="mx-auto mb-20 max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
+        <div className="mx-auto my-20 max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:items-center md:gap-8">
             <div>
               <div className="max-w-lg md:max-w-none">
@@ -40,7 +50,7 @@ export default function Page() {
                 </h2>
 
                 <p className="mt-4 text-gray-700">
-                GlobeGuard keeps you informed with real-time safety updates from trusted government sources, helping you travel smart and confident
+                  GlobeGuard keeps you informed with real-time safety updates from trusted government sources, helping you travel smart and confident
                 </p>
               </div>
             </div>
@@ -51,7 +61,7 @@ export default function Page() {
                 className="rounded"
                 width={1000}
                 height={1000}
-                 alt=""
+                alt=""
               />
             </div>
           </div>
@@ -69,7 +79,7 @@ export default function Page() {
                 </h2>
 
                 <p className="mt-4 text-gray-700">
-                Culture Compass helps you navigate key cultural and infrastructure differences across countries. Whether it’s which side of the road people drive on, the type of electrical outlets you’ll need, or local customs you should know, we make it easy to prepare for your journey. Explore the world with confidence
+                  Culture Compass helps you navigate key cultural and infrastructure differences across countries. Whether it’s which side of the road people drive on, the type of electrical outlets you’ll need, or local customs you should know, we make it easy to prepare for your journey. Explore the world with confidence
                 </p>
               </div>
             </div>
@@ -98,7 +108,7 @@ export default function Page() {
                 </h2>
 
                 <p className="mt-4 text-gray-700">
-                Traveling alone doesn’t mean you’re on your own. SoloNet helps you connect, share, and support each other on your journeys
+                  Traveling alone doesn’t mean you’re on your own. SoloNet helps you connect, share, and support each other on your journeys
                 </p>
               </div>
             </div>
