6bbb7d751f62363a9778b9edf35ad0535eb42abb
chwrd <hocheng06@gmail.com>
2025-04-26 17:56:55 -0700

    added sign in with google button, cleaned up styling

--- solo-traveler/src/app/login/page.js
+++ solo-traveler/src/app/login/page.js
@@ -22,7 +22,7 @@ export default function LoginPage() {
       className="w-full h-screen bg-cover bg-center flex items-center justify-center"
       style={{ backgroundImage: "url('/woman.jpg')" }}
     >
-      <div className="flex w-full bg-[#F9D5D3] justify-between shadow-md rounded-xl mt-15 mb-15 ml-50 mr-50">
+      <div className="flex  bg-[#F9D5D3] justify-center shadow-md rounded-xl p-15">
         <form className="max-w-s px-12 pt-15 pb-15 mb-4">
           <h1
             className={`${juliusSansOne.className} text-black text-4xl text-center`}
@@ -32,52 +32,67 @@ export default function LoginPage() {
           <h2 className={`${istokWeb.className}, text-center mt-4 mb-4`}>
             Welcome back traveller
           </h2>
-          <div className="mb-4">
-            <input
-              className="bg-white shadow appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="email"
-              type="email"
-              placeholder="Email"
-            />
-          </div>
-          <div className="mb-6">
-            <input
-              className="bg-white shadow appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="password"
-              type="password"
-              placeholder="Password"
-            />
-            <p className="text-red-500 text-xs italic float-right">
-              Forget password?
-            </p>
+
+          {/* Login */}
+          <div>
+            <div className="mb-4">
+              <input
+                className="bg-white shadow appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="email"
+                type="email"
+                placeholder="Email"
+              />
+            </div>
+            <div className="mb-6">
+              <input
+                className="bg-white shadow appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="Password"
+              />
+              <p className="text-red-500 text-xs italic float-right">
+                Forget password?
+              </p>
+            </div>
+            <div className="flex justify-center items-center">
+              <button
+                className="bg-[#C14455] hover:bg-[#962a39] w-full text-white font-bold py-2 px-4 rounded-2xl focus:outline-none focus:shadow-outline text-content"
+                type="button"
+              >
+                Log In
+              </button>
+            </div>
+
+            {/* Divider */}
             <div className="inline-flex items-center justify-center w-full relative">
               <hr className="w-64 h-px my-8 border-0 bg-black"></hr>
               <span className="absolute px-3 font-medium text-gray-900 -translate-x-1/2 left-1/2 bg-[#F9D5D3] dark:text-black ">
                 or
               </span>
             </div>
-            <a
-              onClick={() => signIn("google", { callbackUrl: "/" })}
-              className={`${istokWeb.className} text-blue-700 text-center underline`}
-            >
-              Log in with Google
-            </a>
-          </div>
-          <div className="flex justify-center items-center">
-            <button
-              className="bg-[#C14455] hover:bg-[#962a39] w-full text-white font-bold py-2 px-4 rounded-2xl focus:outline-none focus:shadow-outline text-content"
-              type="button"
-            >
-              Log In
-            </button>
+
+            {/* Login with Google */}
+            <div className="flex justify-center items-center ">
+              <a
+                onClick={() => signIn("google", { callbackUrl: "/" })}
+                className={`${istokWeb.className} text-center align`}
+              >
+                <button class="bg-white px-4 py-2 border flex gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150">
+                  <img class="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo"></img>
+                  <span className="text-black">Login with Google</span>
+                </button>
+              </a>
+            </div>
           </div>
         </form>
+
+        {/* Card image */}
         <img
           src="/woman.jpg"
           alt="Background image of a woman"
           width={300}
           height={300}
-          className="p-3 rounded-lg object-cover"
+          className="p-3 rounded-xl object-cover"
         />
       </div>
     </div>
