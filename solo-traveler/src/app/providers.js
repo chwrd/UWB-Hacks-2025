d0dc9a7e7e4b402972336483347618af187feb1b
Howard <102770528+chwrd@users.noreply.github.com>
2025-04-28 01:01:29 -0700

    Delete solo-traveler directory

--- solo-traveler/src/app/providers.js
+++
@@ -1,8 +0,0 @@
-// app/providers.js (Client Component)
-"use client"; // This makes it a Client Component
-
-import { SessionProvider } from "next-auth/react";
-
-export default function Providers({ children }) {
-  return <SessionProvider>{children}</SessionProvider>;
-}
